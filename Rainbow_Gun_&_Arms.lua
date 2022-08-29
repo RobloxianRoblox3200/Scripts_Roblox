f23c25e2668eccf9218eb097b79be9e0391305a9
Robloxian <92522487+RobloxianRoblox3200@users.noreply.github.com>
2022-08-29 14:12:55 +0300

    Scripts

---
+++ Rainbow_Gun_&_Arms.lua
@@ -0,0 +1,15 @@
+--BROUGHT TO YOU BY RSCRIPTS.NET--
+
+Blacklisted = {"Flame", "SightMark", "SightMark2A", "Tip", "Trigger"}
+
+workspace.CurrentCamera.DescendantAdded:Connect(function(O)
+    if O:IsA("BasePart") and not table.find(Blacklisted, O.Name) then
+        spawn(function()
+            while wait() do
+                O.Color = Color3.fromHSV((tick() % 5 / 5), 1, 1)
+                O.Transparency = 0.8
+                O.Material = Enum.Material.ForceField
+            end
+        end)
+    end
+end)
