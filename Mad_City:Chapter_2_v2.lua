7e518b37d6aac1a9d12f2da94a33ce35142766ae
Robloxian <92522487+RobloxianRoblox3200@users.noreply.github.com>
2022-10-14 00:26:05 +0400

    Scripts

---
+++ Mad_City:Chapter_2_v2.lua
@@ -0,0 +1,9 @@
+getgenv().fov = 140 -- Field of View: The silent aim is only targetted at the target inside the fov's radius.
+getgenv().bodypart = "Head" -- Targetting: "Head", "Torso". For example: Using "Head" will only deal headshots.
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RobloxianRoblox3200/Scripts_Roblox/main/Mad_City%3A_Chapter_2.lua', true))()
+
+--[[
+getgenv().fov = 260
+getgenv().bodypart = "Head"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Cesare0328/my-scripts/main/SAMCH2', true))()
+]]--
